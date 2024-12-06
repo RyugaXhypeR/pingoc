257f827708962017d2eacce30fa63d4f7336f545
RyugaXhypeR <ryugashyper@gmail.com>
2024-12-06 17:35:31 +0530

    feat: added `recursive_lookup`

--- src/dns/resolve.rs
+++ src/dns/resolve.rs
@@ -1,6 +1,8 @@
+use crate::dns::header::ResponseCode;
+
 use super::{buffer::PacketBuffer, packet::Packet, query::QueryType, question::Question};
 use std::error::Error;
-use std::net::UdpSocket;
+use std::net::{Ipv4Addr, UdpSocket};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
@@ -23,4 +25,40 @@ pub fn lookup(domain: &str, query_type: QueryType, server: (Ipv4Addr, u16)) -> R
     socket.recv_from(&mut buffer.buffer)?;
 
     Packet::read(&mut buffer)
+}
+
+pub fn recursive_lookup(query_name: &str, query_type: QueryType) -> Result<Packet> {
+    let mut nameserver = Ipv4Addr::new(198, 41, 0, 4);
+    loop {
+        if cfg!(debug_assertions) {
+            println!(
+                "Attempting lookup of {query_name}::{query_type:?} with nameserver {nameserver:?}"
+            );
+        }
+
+        let server = (nameserver, 53);
+        let response = lookup(query_name, query_type, server)?;
+
+        if (!response.answers.is_empty() && response.header.response_code == ResponseCode::NoError)
+            || response.header.response_code == ResponseCode::NxDomain
+        {
+            return Ok(response);
+        }
+
+        if let Some(new_nameserver) = response.get_resolved_nameserver(query_name) {
+            nameserver = new_nameserver;
+            continue;
+        }
+
+        let new_nameserver = match response.get_uresolved_nameserver(query_name) {
+            Some(ns) => ns,
+            None => return Ok(response),
+        };
+
+        let recursive_response = recursive_lookup(new_nameserver, QueryType::A)?;
+        match recursive_response.get_a_record() {
+            Some(ns) => nameserver = ns,
+            None => return Ok(response),
+        };
+    }
 }
